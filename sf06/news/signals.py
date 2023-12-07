06302962c9289cd3d9f063238a386c7a249fc365
FomenkoVD <fom.stud@yandex.ru>
2023-12-07 11:52:55 +0500

    1st step ready

--- sf06/news/signals.py
+++ sf06/news/signals.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from django.db.models.signals import m2m_changed, post_save
 from django.dispatch import receiver
-
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
 from .models import Post, Category, PostCategory, Subscription, User
 
 # короче надо со временем связывать, иначе никак
@@ -10,11 +11,20 @@ def subsribed_mail_send(instance, **kwargs):
     if kwargs.get('action')!='post_add':
         return
     timecreate = instance.time_create.replace(tzinfo=None)
-    if ((datetime.utcnow()-timecreate).total_seconds())>1:
+    if ((datetime.utcnow()-timecreate).total_seconds())<1:
         users_id = Subscription.objects.filter(category__in=instance.category.all()).values_list('user', flat=True)
         users = User.objects.filter(pk__in=users_id)
         for user in users:
-            pass
+            if user.email != '':
+                subj = 'Новая новость из Вашего любимого!'
+                text = f'{user.username} у Ваших любимых категорий появилась новая новость!'
+                html = f'{user.username}, у Ваших любимых категорий появилась новая <a href="{settings.SITE_URL}{instance.get_absolute_url()}">новость</a>!'
+                msg = EmailMultiAlternatives(subject=subj, body=text, from_email=settings.EMAIL_HOST_USER,
+                                             to=[user.email])
+                msg.attach_alternative(html, 'text/html')
+                msg.send()
+
+
 
 
 
--- sf06/sf06/settings.py
+++ sf06/sf06/settings.py
@@ -140,7 +140,7 @@ LOGIN_URL = 'accounts/login'
 LOGIN_REDIRECT_URL = "news"
 
 SITE_ID = 1
-
+SITE_URL = 'http://127.0.0.1:8000'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
@@ -151,3 +151,13 @@ ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # для тестов в консоль!!!
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "fom.stud@yandex.ru"
+EMAIL_HOST_PASSWORD = "vpyydvrgmrwxafpl"
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+DEFAULT_FROM_EMAIL = "example@yandex.ru"
