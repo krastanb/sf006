807e4e24749aa30ed6ee479865a4a37ca0f3aab6
FomenkoVD <fom.stud@yandex.ru>
2023-12-07 17:13:57 +0500

    rdy

--- sf06/sf06/settings.py
+++ sf06/sf06/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.yandex',
-    'django.contrib.sites'
+    'django.contrib.sites',
+    'django_apscheduler',
 ]
 
 MIDDLEWARE = [
@@ -152,8 +153,8 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # для тестов в консоль!!!
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # для тестов в консоль!!!
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = "fom.stud@yandex.ru"
